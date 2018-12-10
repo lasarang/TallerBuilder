fc1331af24ecfd81b6496636c877d143dcd82771
luis <lasarang@espol.edu.ec>
2018-12-10 11:24:17 -0500

    Implementación de la clase Main
    Ejecutando y probando el  funcionamiento del proyecto

--- ManejoComputadoras/src/ensamblajecomputadoras/creacional/AsusZenbookBuilder.java
+++ ManejoComputadoras/src/ensamblajecomputadoras/creacional/AsusZenbookBuilder.java
@@ -9,6 +9,7 @@ public class AsusZenbookBuilder extends ComputadorBuilder{
     public void DefinirComputador() {
         c = new Computador();
         c.setRam(16);
+        c.setMarca("Asus");
         c.setModelo("Zenbook");
         c.setAlmacenamiento(500);
         
@@ -17,13 +18,12 @@ public class AsusZenbookBuilder extends ComputadorBuilder{
     @Override
     public void ConstruirSO() {
     	 c.setOs(new SistemaOperativo("Windows 10",64, "Home Edition" ));
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
     public void ConstruirMainboard() {
     	c.setPlaca(new Mainboard("Prime","Z370"));
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     @Override
     public void DefinirExtras() {
--- ManejoComputadoras/src/ensamblajecomputadoras/creacional/Main.java
+++ ManejoComputadoras/src/ensamblajecomputadoras/creacional/Main.java
@@ -3,6 +3,21 @@ package ensamblajecomputadoras.creacional;
 public class Main {
 	
 	public static void main(String[] a) {
+		ComputadorDirector directorAsusRoge= new ComputadorDirector(new AsusROGEBuilder());
+		directorAsusRoge.ConstruirComputador();
+		Computador Roge= directorAsusRoge.getComputador();
+		
+		ComputadorDirector directorAsusZenbook= new ComputadorDirector(new AsusZenbookBuilder());
+		directorAsusZenbook.ConstruirComputador();
+		Computador Zenbook= directorAsusZenbook.getComputador();
+		
+		System.out.println("Primer computador: "+Roge);
+		System.out.println(Roge.especificacionesTecnicas());
+		
+		System.out.println("Segundo computador: "+Zenbook);
+		System.out.println(Zenbook.especificacionesTecnicas());
+		
+		
 		
 		
 		
